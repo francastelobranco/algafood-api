8eabc9902fcedb192d63e4891e9a64d1a67721a3
francastelobranco <francy.cb2@gmail.com>
2025-01-13 10:56:38 -0300

    Externalizando consultas JPQL para um arquivo XML

--- src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
@@ -23,6 +23,6 @@ public interface RestauranteRepository extends JpaRepository<Restaurante, Long> 
 
     Integer countByCozinhaId(Long cozinhaId);
 
-    @Query("from Restaurante where nome like %:nome% and cozinha.id = :id")
+    // @Query("from Restaurante where nome like %:nome% and cozinha.id = :id")
     List<Restaurante> consultarPorNome(String nome, @Param("id") Long cozinhaId);
 }
